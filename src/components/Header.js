01ecdf9c371db21a3c48fae1f6b10f63cad3d974
nacho <colba@MacBook-Pro-de-nacho.local>
2019-08-06 09:57:50 +0200

    creating categories provider

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () => (
+  <header className="uk-margin" uk-margin="true">
+    <h1 className="uk-text-center">Events app</h1>
+  </header>
+);
+export default Header;
---
+++ src/context/CategoriesContext.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+// create context
+const CategoriesContext = React.createContext();
+
+export const CategoriesConsumer = CategoriesContext.Consumer;
+
+class CategoriesProvider extends Component {
+  token = "XNDEQ43UPSBBM3B4ZRGD";
+  state = {
+    categories: []
+  };
+
+  componentDidMount() {
+    this.getCategories();
+  }
+
+  getCategories = async () => {
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=${
+      this.token
+    }&locale=es_ES`;
+
+    let categories = await axios.get(url);
+
+    this.setState({
+      categories: categories.data.categories
+    });
+  };
+
+  render() {
+    return (
+      <CategoriesContext.Provider value={{ categories: this.state.categories }}>
+        {this.props.children}
+      </CategoriesContext.Provider>
+    );
+  }
+}
+export default CategoriesProvider;
